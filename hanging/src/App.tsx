5a743680cc39bc3e4a66e354e0399839c42c69b5
Jeison Anillo <jeisondejesushotmail.es>
2024-05-04 11:02:35 -0500

    lose vs win

--- hanging/src/App.tsx
+++ hanging/src/App.tsx
@@ -1,7 +1,7 @@
 import {letters} from './helpers/letters';
 import {HangImage} from './components/HangImage';
 import './App.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
@@ -9,8 +9,26 @@ function App() {
   const [ word ] = useState('COMPONENT');
   const [ hiddenWord, setHiddenWord ] = useState( '_ '.repeat( word.length ) );
   const  [attempts, setAttempts ] = useState(0);
+  const [ lose, setLose ] = useState(false);
+  const [ won, setWon ] = useState(false);
+
+  useEffect(() => {
+    if(attempts >= 9){
+      setLose(true);
+    }
+  }, [ attempts ] );
+
+  useEffect(() => {
+    const currentHiddenWord = hiddenWord.split(' ').join('');
+    if(currentHiddenWord === word){
+      setWon(true);    
+    }
+  }, [ hiddenWord ] );
+
 
   const checkLetter = (letter: string) => {
+    
+    if (lose) return;
     
     if( !word.includes(letter) ){
       setAttempts( Math.min( attempts + 1, 9 ));
@@ -40,7 +58,23 @@ function App() {
         
         { /* Counter attempts */}
         <h3> Attempts: {attempts} </h3>
-      
+
+        {/* Alerting a message if the participant lost*/}
+
+        {
+
+          (lose) 
+          ? <h3>You lose! <h3>Here it is the word: {word} you were supossed to guess </h3></h3> : ''
+        }
+        
+        {/* Alerting a message if the participant Won*/}
+
+        {
+
+        (won) 
+        ? <h3>Congrats!. You won! <h3>Here it is the word: {word} you guessed </h3></h3> : ''
+        }
+
         { /* Letters buttons */}
         
         {
